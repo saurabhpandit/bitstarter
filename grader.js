ce25ab237210742b81b95afb267413cf1917bf0c
Saurabh@startup <saurabh.c.pandit@gmail.com>
2013-07-14 12:18:37 +0000

    hw3

--- grader.js
+++ grader.js
@@ -22,9 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://tranquil-wave-3809.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,16 +39,21 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+
+var cheerioReadHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioReadHtmlString = function(htmlstring) {
+    return cheerio.load(htmlstring);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(readFunc, htmlfile, checksfile) {
+    $ = readFunc(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -55,6 +63,9 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function() {
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -62,13 +73,29 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
+    var checkJson, outJson;
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to html page')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+        rest.get(program.url).on('complete', function(result, data){
+            if (result instanceof Error) {
+                console.error('Error: ' + util.format(response.message));
+            } else {
+                checkJson = checkHtmlFile(cheerioReadHtmlString, data, program.checks);
+                outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+
+        }); 
+    }
+    else {
+        checkJson = checkHtmlFile(cheerioReadHtmlFile, program.file, program.checks);
+        outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
